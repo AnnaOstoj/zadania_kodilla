0d3d30b80aeb808630c78163076c21d84e0ce346
Anna Ostojska <anna.ostojskaa@gmail.com>
2020-06-24 16:01:24 +0200

    Description added

--- palindromy.py
+++ palindromy.py
@@ -1,4 +1,8 @@
 def palindromy(slowo):
+    """
+    Check if a word passed as input is a palindrome (a word read from left to right and backwards gives is the same word)
+    Function returns a flag as output. If word is a palinddrome, then function returns "true"
+    """
     is_palindrom = True
     dl_slowo = len(slowo)
     for i in range(dl_slowo):
