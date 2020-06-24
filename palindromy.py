537ba01c4ad4a105850fb981a6ce27806236895c
Anna Ostojska <anna.ostojskaa@gmail.com>
2020-06-24 15:52:56 +0200

    Check capital letter

--- palindromy.py
+++ palindromy.py
@@ -1,11 +1,9 @@
 def palindromy(slowo):
-    flaga = "True"
+    is_palindrom = True
     dl_slowo = len(slowo)
     for i in range(dl_slowo):
-        print(slowo[i],slowo[dl_slowo-i-1])
-        if slowo[i] != slowo[len(slowo)-i-1]:
-            flaga = "False"
-    
-    return flaga
+        if str.lower(slowo[i]) != str.lower(slowo[len(slowo)-i-1]):
+            is_palindrom = False
+    return is_palindrom
 
-print(palindromy("kajak"))
+print(palindromy("anna"))
