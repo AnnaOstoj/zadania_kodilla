e615e22f1aeeaaf4d721e9ae69ae5feabaadfb7d
Anna Ostojska <anna.ostojskaa@gmail.com>
2020-06-24 18:25:39 +0200

    Check phrase

--- palindromy.py
+++ palindromy.py
@@ -1,13 +1,18 @@
 def palindromy(slowo):
     """
-    Check if a word passed as input is a palindrome (a word read from left to right and backwards gives is the same word)
+    Check if a word/phrase passed as input is a palindrome (a word read from left to right and backwards gives is the same word)
     Function returns a flag as output. If word is a palinddrome, then function returns "true"
     """
     is_palindrom = True
-    dl_slowo = len(slowo)
+    
+    only_letters = [char for char in slowo if char.isalnum()]
+
+    dl_slowo = len(only_letters)
     for i in range(dl_slowo):
-        if str.lower(slowo[i]) != str.lower(slowo[len(slowo)-i-1]):
+        if str.lower(only_letters[i]) != str.lower(only_letters[len(only_letters)-i-1]):
             is_palindrom = False
     return is_palindrom
 
-print(palindromy("anna"))
+print(palindromy("Łapał za kran, a kanarka złapał"))
+
+
